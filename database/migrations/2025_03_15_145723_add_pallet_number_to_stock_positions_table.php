300738ebb5ba89ff6fd4da8bafc6c7290b1b2294
Yakanaro <kipaherd@gmail.com>
2025-07-05 16:01:44 +0300

    fix migration

--- database/migrations/2025_03_15_145723_add_pallet_number_to_stock_positions_table.php
+++ database/migrations/2025_03_15_145723_add_pallet_number_to_stock_positions_table.php
@@ -6,23 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::table('stock_positions', function (Blueprint $table) {
-            $table->string('pallet_number')->nullable()->after('product_type_id');
-        });
-    }
+    // /**
+    //  * Run the migrations.
+    //  */
+    // public function up(): void
+    // {
+    //     Schema::table('stock_positions', function (Blueprint $table) {
+    //         $table->string('pallet_number')->nullable()->after('product_type_id');
+    //     });
+    // }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('stock_positions', function (Blueprint $table) {
-            $table->dropColumn('pallet_number');
-        });
-    }
+    // /**
+    //  * Reverse the migrations.
+    //  */
+    // public function down(): void
+    // {
+    //     Schema::table('stock_positions', function (Blueprint $table) {
+    //         $table->dropColumn('pallet_number');
+    //     });
+    // }
 };
