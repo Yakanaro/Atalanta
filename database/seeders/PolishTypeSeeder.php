c3a175d085fa4f3ab22f0469a8c8b4c41196d670
Yakanaro <kipaherd@gmail.com>
2025-07-06 22:06:16 +0300

    fix

--- database/seeders/PolishTypeSeeder.php
+++ database/seeders/PolishTypeSeeder.php
@@ -11,32 +11,32 @@ class PolishTypeSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
-        $polishTypes = [
-            [
-                'name' => 'Одна сторона',
-                'sort_order' => 10,
-            ],
-            [
-                'name' => 'Две стороны',
-                'sort_order' => 20,
-            ],
-            [
-                'name' => 'Три стороны',
-                'sort_order' => 30,
-            ],
-            [
-                'name' => 'Четыре стороны',
-                'sort_order' => 40,
-            ],
-        ];
+    // public function run(): void
+    // {
+    //     $polishTypes = [
+    //         [
+    //             'name' => 'Одна сторона',
+    //             'sort_order' => 10,
+    //         ],
+    //         [
+    //             'name' => 'Две стороны',
+    //             'sort_order' => 20,
+    //         ],
+    //         [
+    //             'name' => 'Три стороны',
+    //             'sort_order' => 30,
+    //         ],
+    //         [
+    //             'name' => 'Четыре стороны',
+    //             'sort_order' => 40,
+    //         ],
+    //     ];
 
-        foreach ($polishTypes as $type) {
-            PolishType::firstOrCreate(
-                ['name' => $type['name']],
-                ['sort_order' => $type['sort_order'], 'is_active' => true]
-            );
-        }
-    }
+    //     foreach ($polishTypes as $type) {
+    //         PolishType::firstOrCreate(
+    //             ['name' => $type['name']],
+    //             ['sort_order' => $type['sort_order'], 'is_active' => true]
+    //         );
+    //     }
+    // }
 }
--- database/seeders/ProductTypeSeeder.php
+++ database/seeders/ProductTypeSeeder.php
@@ -11,20 +11,20 @@ class ProductTypeSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
-        $productTypes = [
-            ['name' => 'Стелла'],
-            ['name' => 'Надгробие'],
-            ['name' => 'Подставка'],
-            ['name' => 'Цветник'],
-        ];
+    // public function run(): void
+    // {
+    //     $productTypes = [
+    //         ['name' => 'Стелла'],
+    //         ['name' => 'Надгробие'],
+    //         ['name' => 'Подставка'],
+    //         ['name' => 'Цветник'],
+    //     ];
 
-        foreach ($productTypes as $type) {
-            ProductType::firstOrCreate(
-                ['name' => $type['name']],
-                ['is_active' => true]
-            );
-        }
-    }
+    //     foreach ($productTypes as $type) {
+    //         ProductType::firstOrCreate(
+    //             ['name' => $type['name']],
+    //             ['is_active' => true]
+    //         );
+    //     }
+    // }
 }
