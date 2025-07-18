d2ec1947c076a7f3b9784069ae8358595ec6f899
Yakanaro <kipaherd@gmail.com>
2025-07-18 16:56:28 +0300

    add logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -7,10 +7,10 @@
                 <div class="shrink-0 flex items-center">
                     @auth
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        <x-application-logo class="block object-contain" style="height: 60px; width: 60px;" />
                     </a>
                     @else
-                    <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                    <x-application-logo class="block object-contain" style="height: 60px; width: 60px;" />
                     @endauth
                 </div>
 
