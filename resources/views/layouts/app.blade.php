d00120764b0629d0a3509e4003b9a8b9451479f1
Yakanaro <kipaherd@gmail.com>
2025-08-20 00:16:29 +0300

    Фикс фронта

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,6 +22,8 @@
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.css" rel="stylesheet" />
+
     <!-- Flowbite CSS removed: styles come from Tailwind build via plugin to avoid overriding dark classes -->
 
     <!-- Установка темной темы по умолчанию -->
